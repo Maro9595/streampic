c5bcd071f740325ecec46e374b06319cfa4242ca
Maro9595 <50424979+Maro9595@users.noreply.github.com>
2019-07-11 15:53:49 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+
+client.on("ready", () => {
+    client.user.setPresence({
+      status: 'dnd',
+      game: { 
+         type: 0,
+         name: 'Surprise Mother Fucker! :) ',
+         details: `I'm : LuXy - Developer and Programmer`,
+         url: 'http://twitch.tv/Streammingg',
+         state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+        application_id: '533944821228896257',
+         assets: {
+            small_image: `366835431037337600`,
+            small_text: ' The Best My Love ! ' ,
+            large_image: `370451271133429760`,
+            large_text: `EGYPT The Best Love FOREVER ♥` }
+    
+      }
+        });
+    });
+    
